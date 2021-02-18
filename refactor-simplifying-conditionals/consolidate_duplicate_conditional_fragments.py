54f2057a4306252ade0c92fdf9cd3f1e24eaab49
aucoeur <aucoeur.ngo@students.makeschool.com>
2021-02-18 13:03:47 -0800

    finished consolidate_duplicate_conditional_fragments.py idk what im doing

--- refactor-simplifying-conditionals/consolidate_duplicate_conditional_fragments.py
+++ refactor-simplifying-conditionals/consolidate_duplicate_conditional_fragments.py
@@ -1,3 +1,39 @@
 class Drink:
-    def __init__(self, ingredients):
-        self.ingredients = tuple(ingredients)
+    def __init__(self):
+        self._mix = []
+
+    @property
+    def mix(self):
+        return self._mix
+
+    @mix.setter
+    def mix(self, addons):
+        if len(self.mix) > 1:
+            print(f'mixed {self.mix} with {addons}')
+        self._mix.extend(addons)
+
+class Menu(Drink):
+    def __init__(self, drinks):
+        self._drinks = []
+
+        for drink in drinks:
+            self._drinks.append(drink)
+
+    def make_drink(self, drink):
+        if drink in self._drinks:
+            return drink.mix
+
+coffee = Drink()
+coffee.mix = ["coffee"]
+
+ice_cream = Drink()
+ice_cream.mix = ['ice', 'cream']
+
+strawberry_milkshake = ice_cream
+strawberry_milkshake.mix = ['strawberry', 'milk', 'sugar']
+
+
+menu = Menu([coffee, ice_cream, strawberry_milkshake])
+
+final_drink = menu.make_drink(strawberry_milkshake)
+print(final_drink)
